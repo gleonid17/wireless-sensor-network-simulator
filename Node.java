af2712399bb476db109a5d1fedb34f47b2c98971
gleoni17 <gleonidou17@proton.me>
2026-04-12 21:36:53 +0300

    Started forming class diagram

---
+++ Node.java
@@ -0,0 +1,39 @@
+public class Node {
+    private String id;
+    private Position position;
+    private double temperature;
+    private boolean isFireStation; //determined by ID starting with "PS"
+
+    public Node(String id, double x, double y, double temperature) {
+
+    }
+
+    public String getID() {
+        
+    }
+
+    public String getID() {
+
+    }
+
+    public Position getPosition() {
+
+    }
+
+    public double getTemperature() {
+
+    }
+
+    public void setTemperature(double temperature) {
+
+    }
+
+    public boolean isFireStation() {
+
+    }
+
+    @Override
+    public String toString() {
+
+    }
+}
---
+++ Position.java
@@ -0,0 +1,31 @@
+public class Position {
+    private double x;
+    private double y;
+
+    public Position(double x, double y) {
+
+    }
+
+    public double getX() {
+
+    }
+
+    public double getY() {
+
+    }
+
+    public static double distance(Position a, Position b) {
+
+    }
+
+    public static boolean withinRange(Position a, Position b, double d) {
+
+    }
+
+    @Override
+    public String toString() {
+        
+    }
+}
+    
+    
